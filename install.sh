95be1778ca6e4b56cf00feefa2dd182806e5869b
Antoine Deleporte <adeleporte@vmware.com>
2021-05-12 12:58:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 docker run -d --name mongo -p 27017:27017 mongo
 curl https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.ova --output bionic-server-cloudimg-amd64.ova
-docker run -d --rm --name easyavi --link mongo -p 443:443 -v "$PWD":/root/images --add-host dockerhost:`ip -f inet addr show eth0 | awk '/inet / {print $2}' | awk -F/ '{print $1}'`  adeleporte/easyavi
+docker run -d --rm --name easyavi --link mongo -p 443:443 -v "$PWD":/root/images --add-host dockerhost:`ip -f inet addr show eth0 | awk '/inet / {print $2}' | awk -F/ '{print $1}'` --env HOST_MAC=`ip link show eth0 | grep ether | awk '{print $2}'`  adeleporte/easyavi
